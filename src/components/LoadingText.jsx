0875de936b66feda954001b4891aa3d514c96c29
Greg <gmerrill03@gmail.com>
2019-10-01 16:44:18 -0700

    add loading text

---
+++ src/components/LoadingText.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class LoadingText extends Component {
+
+    // componentWillUnmount() {
+    //     this.setState({ isLoading: true });
+    // }
+    
+    render() {
+        return (
+            <div>Loading...</div>
+        );
+    }
+}
+
+export default LoadingText;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import { getAllStarships } from '../services/sw-api';
-import { Route, Link } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
+import LoadingText from '../components/LoadingText';
 
 class App extends Component {
 
   state = {
-    starships: []
+    starships: [],
+    isLoading: true
   }
 
   async componentDidMount() {
@@ -18,33 +20,36 @@ class App extends Component {
       starships = [...starships, ...freshStarships.results];
       i++;
     }
-    this.setState({ starships });
+    this.setState({ starships, isLoading: false });
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">STAR WARS STARSHIPS</header>
-        <Route exact path='/' render={() => (
-          <div className="starships-ctnr">
-            {this.state.starships.map((s, idx) => (
-              <Link to={`/${idx}`} key={s.name} className="starship">{s.name}</Link>
-            ))}
-          </div>
-        )}/>
-        <Route path='/:id' render={props => (
-          <div className="starship-detail">
-            <div className="detail-row">
-              <p>NAME:</p>
-              <p>{this.state.starships[props.match.params.id].name}</p>
+        {this.state.isLoading && <LoadingText />}
+        <Switch>
+          <Route exact path='/' render={() => (
+            <div className="starships-ctnr">
+              {this.state.starships.map((s, idx) => (
+                <Link to={`/${idx}`} key={s.name} className="starship">{s.name}</Link>
+              ))}
             </div>
-            <div className="detail-row">
-              <p>MODEL:</p>
-              <p>{this.state.starships[props.match.params.id].model}</p>
+          )}/>
+          <Route path='/:id' render={props => (
+            <div className="starship-detail">
+              <div className="detail-row">
+                <p>NAME:</p>
+                <p>{this.state.starships[props.match.params.id].name}</p>
+              </div>
+              <div className="detail-row">
+                <p>MODEL:</p>
+                <p>{this.state.starships[props.match.params.id].model}</p>
+              </div>
+              <Link to='/' className="return-link">RETURN</Link>
             </div>
-            <Link to='/' className="return-link">RETURN</Link>
-          </div>
-        )}/>
+          )}/>
+        </Switch>
       </div>
     );
   }
